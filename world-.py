f8b4e35bc78ac1ab07b6590d453cf7ed11b0394e
Roz'e leax <roze.leax@gmail.com>
2026-02-16 14:25:32 +0530

    Add files via upload

---
+++ world-.py
@@ -0,0 +1,28 @@
+
+
+class World:
+
+    def __init__(self, width, height, banana_population):
+        self.width = width
+        self.height = height
+        self.population = population = [] 
+
+        for i in range(banana_population):
+            creature = Creature(
+                health = 100,
+                speed = 10,
+                mut_chance = 1,
+                appetite = 100)
+
+            self.population.append(creature)
+
+    def update(self):
+        for creature in self.population:
+            creature.update()
+
+    def display_population(self):
+        print('population size', len(self.population))
+   
+
+
+
