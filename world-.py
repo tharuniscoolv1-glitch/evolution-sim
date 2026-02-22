8d697bd9ba63c1e9a67fc1f745bbccf0b9af1b2a
Roz'e leax <roze.leax@gmail.com>
2026-02-22 21:40:00 +0530

    Enhance World class with speed scoring features
    Added range and score attributes to Creature initialization. Implemented get_cutoff and speed_scoring methods for population speed analysis.

--- world-.py
+++ world-.py
@@ -13,7 +13,9 @@ class World:
                 health = 100,
                 speed = 10,
                 mut_chance = 1,
-                appetite = 100)
+                appetite = 100,
+            range_ = 5, score = 0
+            )
 
             self.population.append(c)
 
@@ -23,7 +25,30 @@ class World:
 
     def display_population(self):
         print('population:', len(self.population))
+
+    def get_cutoff(self):
+        if not self.populationn:    
+            return 0 
+
+        speeds = [creature.speed*creature.range for creature in self.creature ]
+
+        average_speed = statistics.mean(speeds)
+
+        cutoff = average_speed*1.5
+        return cutoff 
+
+    def speed_scoring(self):
+        cutoff = self.get_cutoff()
+
+        for creature in self.population:
+            if creature.speed*creature.range > cutoff:
+                creature.score = creature.score + 1
+
+
+    
+    
    
+
 
 
 
